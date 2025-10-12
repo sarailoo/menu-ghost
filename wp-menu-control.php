fe788c02b2c8b52e787483e39ebdf407541ceb49
Reza Sarailoo <sarailoo.reza@gmail.com>
2025-10-12 23:03:18 +0330

    chore: refresh plugin bootstrap file

--- wp-menu-control.php
+++ wp-menu-control.php
@@ -1,7 +1,4 @@
 <?php
-
-declare(strict_types=1);
-
 /**
  * Plugin Name: WP Menu Control
  * Plugin URI: https://github.com/sarailoo/wp-menu-control
@@ -13,53 +10,47 @@ declare(strict_types=1);
  * License: GPL v2 or later
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain: wp-menu-control
+ *
+ * @package WPMenuControl
+ */
+
+declare(strict_types=1);
+
+/**
+ * Bootstrap for the WP Menu Control plugin.
+ *
+ * @package WPMenuControl
  */
 
 namespace WPMenuControl;
 
-if (! class_exists(WPMenuControl::class) && is_readable(__DIR__ . '/vendor/autoload.php')) {
-    require_once __DIR__ . '/vendor/autoload.php';
+if ( ! class_exists( WPMenuControl::class ) && is_readable( __DIR__ . '/vendor/autoload.php' ) ) {
+	require_once __DIR__ . '/vendor/autoload.php';
 }
 
 // Plugin version.
-if (! defined('WP_MENU_CONTROL_VERSION')) {
-    define('WP_MENU_CONTROL_VERSION', '1.0.0');
-}
-
-// Define plugin main file path.
-if (! defined('WP_MENU_CONTROL_PLUGIN_FILE')) {
-    define('WP_MENU_CONTROL_PLUGIN_FILE', __FILE__);
+if ( ! defined( 'WP_MENU_CONTROL_VERSION' ) ) {
+	define( 'WP_MENU_CONTROL_VERSION', '1.0.0' );
 }
 
 // Plugin directory.
-if (! defined('WP_MENU_CONTROL_DIR')) {
-    define('WP_MENU_CONTROL_DIR', plugin_dir_path(__FILE__));
+if ( ! defined( 'WP_MENU_CONTROL_DIR' ) ) {
+	define( 'WP_MENU_CONTROL_DIR', plugin_dir_path( __FILE__ ) );
 }
 
 // Plugin url.
-if (! defined('WP_MENU_CONTROL_URL')) {
-    define('WP_MENU_CONTROL_URL', plugin_dir_url(__FILE__));
-}
-
-// Assets url.
-if (! defined('WP_MENU_CONTROL_ASSETS_URL')) {
-    define('WP_MENU_CONTROL_ASSETS_URL', WP_MENU_CONTROL_URL . '/assets');
+if ( ! defined( 'WP_MENU_CONTROL_URL' ) ) {
+	define( 'WP_MENU_CONTROL_URL', plugin_dir_url( __FILE__ ) );
 }
 
 // Build directory.
-if (! defined('WP_MENU_CONTROL_BUILD_DIR')) {
-    define('WP_MENU_CONTROL_BUILD_DIR', WP_MENU_CONTROL_DIR . 'build/');
+if ( ! defined( 'WP_MENU_CONTROL_BUILD_DIR' ) ) {
+	define( 'WP_MENU_CONTROL_BUILD_DIR', WP_MENU_CONTROL_DIR . 'build/' );
 }
 
 // Build url.
-if (! defined('WP_MENU_CONTROL_BUILD_URL')) {
-    define('WP_MENU_CONTROL_BUILD_URL', WP_MENU_CONTROL_URL . 'build/');
+if ( ! defined( 'WP_MENU_CONTROL_BUILD_URL' ) ) {
+	define( 'WP_MENU_CONTROL_BUILD_URL', WP_MENU_CONTROL_URL . 'build/' );
 }
 
-// Conditions option name.
-if (! defined('WP_MENU_CONTROL_OPTION_NAME')) {
-    define('WP_MENU_CONTROL_OPTION_NAME', 'wp_menu_control_conditions');
-}
-
-class_exists(WPMenuControl::class) && WPMenuControl::instance();
-?>
+class_exists( WPMenuControl::class ) && WPMenuControl::instance();
