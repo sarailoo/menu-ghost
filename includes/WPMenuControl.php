0424ba0654f3362e681dc1f0e621be9bd1b72543
Reza Sarailoo <sarailoo.reza@gmail.com>
2025-10-12 23:02:59 +0330

    refactor: add new plugin bootstrap in includes

---
+++ includes/WPMenuControl.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * Main plugin bootstrap.
+ *
+ * @package WPMenuControl
+ */
+
+declare(strict_types=1);
+
+namespace WPMenuControl;
+
+use WPMenuControl\Admin\Assets\AdminAssets;
+use WPMenuControl\Admin\MenuItem;
+use WPMenuControl\Admin\AdvancedController;
+use WPMenuControl\Admin\SettingsController;
+use WPMenuControl\Admin\SearchController;
+use WPMenuControl\Frontend\MenuVisibility;
+
+/**
+ * WPMenuControl class.
+ */
+class WPMenuControl {
+	/**
+	 * Shared instance of the plugin bootstrap.
+	 *
+	 * @var WPMenuControl|null
+	 */
+	private static ?WPMenuControl $instance = null;
+
+	/**
+	 * Return an instance of the WPMenuControl Class.
+	 *
+	 * @return WPMenuControl class instance
+	 */
+	public static function instance(): WPMenuControl {
+		if ( null === self::$instance ) {
+			self::$instance = new self();
+			self::$instance->init();
+		}
+
+		return self::$instance;
+	}
+
+	/**
+	 * Initialize plugin.
+	 *
+	 * @return void
+	 */
+	public function init() {
+		$this->add_hooks();
+
+		AdminAssets::register();
+		SearchController::register();
+
+		if ( ! is_admin() ) {
+			MenuVisibility::instance();
+
+			return;
+		}
+
+		MenuItem::instance();
+		AdvancedController::instance();
+		SettingsController::instance();
+	}
+
+	/**
+	 * Add hooks.
+	 *
+	 * @return void
+	 */
+	private function add_hooks() {
+		add_action( 'init', array( $this, 'load_text_domain' ) );
+	}
+
+	/**
+	 * Load text domain.
+	 *
+	 * @return void
+	 */
+	public function load_text_domain(): void {
+		load_plugin_textdomain( 'wp-menu-control', false, WP_MENU_CONTROL_DIR . 'languages' );
+	}
+}
